a617ad7fa0d83a147e535551a047f93dd613af4c
Rafal Bartoszak <46748695+rafal2808@users.noreply.github.com>
2024-11-15 11:57:55 +0100

    Add files via upload

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "D:/Elektronika/FPGA/projekty/xilinx/cpu/DCEQ816zip/DCEQ816/ALU_test_isim_beh.exe" -prj "D:/Elektronika/FPGA/projekty/xilinx/cpu/DCEQ816zip/DCEQ816/ALU_test_beh.prj" "work.ALU_test" 
